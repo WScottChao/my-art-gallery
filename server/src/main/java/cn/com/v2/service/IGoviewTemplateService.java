183f281526d5721b98c67196885240e540668257
Jessege <3303749833@qq.com>
2024-12-16 23:36:45 -0500

    backEndfinal

--- server/src/main/java/cn/com/v2/service/IGoviewTemplateService.java
+++ server/src/main/java/cn/com/v2/service/IGoviewTemplateService.java
@@ -1,12 +1,7 @@
 package cn.com.v2.service;
 
-import cn.com.v2.model.GoviewProject;
 import cn.com.v2.model.GoviewTemplate;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.IService;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * <p>
--- server/src/main/java/cn/com/v2/service/impl/GoviewTemplateDataServiceImpl.java
+++ server/src/main/java/cn/com/v2/service/impl/GoviewTemplateDataServiceImpl.java
@@ -1,14 +1,9 @@
 package cn.com.v2.service.impl;
 
-import cn.com.v2.mapper.GoviewProjectDataMapper;
 import cn.com.v2.mapper.GoviewTemplateDataMapper;
-import cn.com.v2.model.GoviewProjectData;
 import cn.com.v2.model.GoviewTemplateData;
-import cn.com.v2.service.IGoviewProjectDataService;
 import cn.com.v2.service.IGoviewTemplateDataService;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
